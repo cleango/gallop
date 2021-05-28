463d108815070b8472920bc7cb75ede92734dea5
larry <jclarry@qq.com>
2021-05-28 15:58:12 +0800

    修改路由绑定错误

--- route.go
+++ route.go
@@ -42,17 +42,17 @@ func (group *Group) Handle(httpMethod, relativePath string, handlers ...interfac
 
 // POST is a shortcut for router.Handle("POST", path, handle).
 func (group *Group) POST(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodPost, relativePath, handlers)
+	return group.Handle(http.MethodPost, relativePath, handlers...)
 }
 
 // GET is a shortcut for router.Handle("GET", path, handle).
 func (group *Group) GET(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodGet, relativePath, handlers)
+	return group.Handle(http.MethodGet, relativePath, handlers...)
 }
 
 // DELETE is a shortcut for router.Handle("DELETE", path, handle).
 func (group *Group) DELETE(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodDelete, relativePath, handlers)
+	return group.Handle(http.MethodDelete, relativePath, handlers...)
 }
 
 // PATCH is a shortcut for router.Handle("PATCH", path, handle).
@@ -62,17 +62,17 @@ func (group *Group) PATCH(relativePath string, handlers ...interface{}) *Group {
 
 // PUT is a shortcut for router.Handle("PUT", path, handle).
 func (group *Group) PUT(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodPut, relativePath, handlers)
+	return group.Handle(http.MethodPut, relativePath, handlers...)
 }
 
 // OPTIONS is a shortcut for router.Handle("OPTIONS", path, handle).
 func (group *Group) OPTIONS(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodOptions, relativePath, handlers)
+	return group.Handle(http.MethodOptions, relativePath, handlers...)
 }
 
 // HEAD is a shortcut for router.Handle("HEAD", path, handle).
 func (group *Group) HEAD(relativePath string, handlers ...interface{}) *Group {
-	return group.Handle(http.MethodHead, relativePath, handlers)
+	return group.Handle(http.MethodHead, relativePath, handlers...)
 }
 
 // HEAD is a shortcut for router.Handle("HEAD", path, handle).
@@ -83,14 +83,14 @@ func (group *Group) Native() *gin.RouterGroup {
 // Any registers a route that matches all the HTTP methods.
 // GET, POST, PUT, PATCH, HEAD, OPTIONS, DELETE, CONNECT, TRACE.
 func (group *Group) Any(relativePath string, handlers ...interface{}) *Group {
-	group.Handle(http.MethodGet, relativePath, handlers)
-	group.Handle(http.MethodPost, relativePath, handlers)
-	group.Handle(http.MethodPut, relativePath, handlers)
-	group.Handle(http.MethodPatch, relativePath, handlers)
-	group.Handle(http.MethodHead, relativePath, handlers)
-	group.Handle(http.MethodOptions, relativePath, handlers)
-	group.Handle(http.MethodDelete, relativePath, handlers)
-	group.Handle(http.MethodConnect, relativePath, handlers)
-	group.Handle(http.MethodTrace, relativePath, handlers)
+	group.Handle(http.MethodGet, relativePath, handlers...)
+	group.Handle(http.MethodPost, relativePath, handlers...)
+	group.Handle(http.MethodPut, relativePath, handlers...)
+	group.Handle(http.MethodPatch, relativePath, handlers...)
+	group.Handle(http.MethodHead, relativePath, handlers...)
+	group.Handle(http.MethodOptions, relativePath, handlers...)
+	group.Handle(http.MethodDelete, relativePath, handlers...)
+	group.Handle(http.MethodConnect, relativePath, handlers...)
+	group.Handle(http.MethodTrace, relativePath, handlers...)
 	return group
 }
